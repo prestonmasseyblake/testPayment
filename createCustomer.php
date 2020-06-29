80e4f2bf504995ba3023b04a841f0f6f3e79b8e8
prestonmasseyblake <54560022+prestonmasseyblake@users.noreply.github.com>
2020-06-28 20:46:43 -0700

    Update createCustomer.php

--- createCustomer.php
+++ createCustomer.php
@@ -3,18 +3,27 @@
 
 
 require_once('./stripe-php/init.php');
-\Stripe\Stripe::setApiKey('sk_test_51GykVOL0FoRfFSl3s6QVaa3ttDgpWniCq7qJAkace4uN5qzLVLSExIWyWEUdO8h1ZiwrUPOMW6OzmGYxV9tGLO2j00Lmtflx8O');
 
-
-$email =  $_POST['email'] ;
-$fullName = $_POST['name'];
-$phone	= $_POST['phone'];
-
-$key = \Stripe\Customer::create([
-  'description' => 'testing','email'=>"$email",'phone'=>"$phone", 'name'=>$fullName
+$stripe = new \Stripe\StripeClient(
+  'sk_test_51GykVOL0FoRfFSl3s6QVaa3ttDgpWniCq7qJAkace4uN5qzLVLSExIWyWEUdO8h1ZiwrUPOMW6OzmGYxV9tGLO2j00Lmtflx8O'
+);
+$stripe->customers->create([
+  'description' => 'My First Test Customer (created for API docs)',
 ]);
 
 
-echo json_encode($key);
+// \Stripe\Stripe::setApiKey('sk_test_51GykVOL0FoRfFSl3s6QVaa3ttDgpWniCq7qJAkace4uN5qzLVLSExIWyWEUdO8h1ZiwrUPOMW6OzmGYxV9tGLO2j00Lmtflx8O');
+
+
+//$email =  $_POST['email'] ;
+//$fullName = $_POST['name'];
+//$phone	= $_POST['phone'];
+
+//$key = \Stripe\Customer::create([
+//  'description' => 'testing','email'=>"$email",'phone'=>"$phone", 'name'=>$fullName
+//]);
+
+
+echo json_encode($stripe);
 
 ?>
